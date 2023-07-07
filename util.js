0f54bb060c937f60766149e66f153a4352137226
Mostafa <mostafaasd2018@gmail.com>
2023-07-07 10:00:41 +0300

    Testing Async Code

--- util.js
+++ util.js
@@ -1,36 +1,19 @@
-const generateText = (name, age) => {
-  // Returns output text
-  return `${name} (${age} years old)`;
+const { fetchData } = require("./http");
+
+const loadTitle = () => {
+  return fetchData().then((extractedData) => {
+    const title = extractedData.title;
+    const transformedTitle = title.toUpperCase();
+    return transformedTitle;
+  });
 };
 
-const createElement = (type, text, className) => {
-  // Creates a new HTML element and returns it
-  const newElement = document.createElement(type);
-  newElement.classList.add(className);
-  newElement.textContent = text;
-  return newElement;
+const printTitle = () => {
+  loadTitle().then((title) => {
+    console.log(title);
+    return title;
+  });
 };
 
-exports.validateInput = (text, notEmpty, isNumber) => {
-  // Validate user input with two pre-defined rules
-  if (!text) {
-    return false;
-  }
-  if (notEmpty && text.trim().length === 0) {
-    return false;
-  }
-  return !(isNumber && isNaN(+text));
-};
 
-exports.checkAndGenerate = (name, age) => {
-  if (
-    !this.validateInput(name, true, false) ||
-    !this.validateInput(age, false, true)
-  ) {
-    return false;
-  }
-  return this.generateText(name, age);
-}
-
-exports.createElement = createElement;
-exports.generateText = generateText;
+exports.printTitle = printTitle;
--- util.test.js
+++ util.test.js
@@ -1,48 +1,6 @@
-const { generateText, checkAndGenerate } = require("./util");
-const puppeteer = require("puppeteer");
+const {printTitle} = require('./util');
 
-// unit tests for generateText function
+test("should print an uppercase text",()=>{
+  expect(printTitle()).toBe("DELECTUS AUT AUTEM");
+})
 
-test("should output name and age", () => {
-  const text = generateText("Max", 29);
-  expect(text).toBe("Max (29 years old)");
-  const text2 = generateText("Anna", 28);
-  expect(text2).toBe("Anna (28 years old)");
-});
-
-test("should output data-less text", () => {
-  const text = generateText();
-  expect(text).toBe("undefined (undefined years old)");
-  const text2 = generateText("", null);
-  expect(text2).toBe(" (null years old)");
-});
-
-// integration tests for checkAndGenerate function
-
-test("should generate a valid text output", () => {
-  const text = checkAndGenerate("Max", 29);
-  expect(text).toBe("Max (29 years old)");
-  const emptyText = checkAndGenerate("", 29);
-  expect(emptyText).toBe(false);
-});
-
-// end-to-end tests
-
-test("should click around", async () => {
-  const browser = await puppeteer.launch({
-    // headless: true,
-    // slowMo: 80,
-    // args: ["--window-size=1920,1080"],
-  });
-
-  const page = await browser.newPage();
-  await page.goto("http://localhost:5500");
-  await page.type("input#name", "Anna");
-  await page.type("input#age", "28");
-  await page.click("#btnAddUser");
-  const result = await page.$eval(
-    ".user-item",
-    (el) => el.textContent === "Anna (28 years old)"
-  );
-  expect(result).toBe(true);
-}, 10000);
